24fff950cc44e8a6707105a0b7c30c9dd98333c0
Fredy <inztockz@gmail.com>
2025-05-12 14:28:25 -0500

    Subiendo

--- src/app/services/tareas.service.ts
+++ src/app/services/tareas.service.ts
@@ -9,7 +9,8 @@ import { TareaRequest } from '../models/tareas/tarea-request';
 })
 export class TareasService {
 
-  private tareasApi:string = "http://localhost:8080/v1/api/tareas";
+  //private tareasApi:string = "http://localhost:8080/v1/api/tareas";
+  private tareasApi:string = "http://192.168.1.10:8080/v1/api/tareas";
 
   constructor(private http:HttpClient) { }
 
